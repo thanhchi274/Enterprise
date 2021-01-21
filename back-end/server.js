410bff1ae8e950b6abdcc481b0f9607f85b20ae1
thanhchi274 <anarapham274@gmail.com>
2021-01-21 19:06:01 +0700

    hello world

---
+++ back-end/server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+app.listen(port, (error) => {
+          if (error) throw error;
+          console.log("Server is running 🚀 " + port);
+});
