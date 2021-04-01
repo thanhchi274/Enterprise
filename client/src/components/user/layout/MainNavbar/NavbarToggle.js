19fd5c0a2348253ecbb002bb19c43d1bdda94491
thanhchi274 <anarapham274@gmail.com>
2021-04-01 18:17:14 +0700

    Hot Fix NavBar Toggle

--- client/src/components/user/layout/MainNavbar/NavbarToggle.js
+++ client/src/components/user/layout/MainNavbar/NavbarToggle.js
@@ -1,41 +1,21 @@
 import React from "react";
-
 import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
-import { selectMenuVisible } from "../../../../Store/UI/UI.selector";
 import { setMenuVisible } from "../../../../Store/UI/UI.action";
 
-class NavbarToggle extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    setMenuVisible();
-  }
-
-  render() {
+const NavbarToggle =({setMenuVisible})=> {
     return (
       <nav className="nav">
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
         <a
           href="#"
-          onClick={this.handleClick}
+          onClick={()=>setMenuVisible()}
           className="nav-link nav-link-icon toggle-sidebar d-sm-inline d-md-inline d-lg-none text-center"
         >
           <i className="material-icons">&#xE5D2;</i>
         </a>
       </nav>
     );
-  }
 }
-
-const mapStateToProps = createStructuredSelector({
-  menuVisible: selectMenuVisible,
-});
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = dispatch => ({
   setMenuVisible: () => dispatch(setMenuVisible()),
 });
-export default connect(mapStateToProps, mapDispatchToProps)(NavbarToggle);
+export default connect(null, mapDispatchToProps)(NavbarToggle);
