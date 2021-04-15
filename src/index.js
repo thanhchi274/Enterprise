c6f000a2b08222e0941a291d25ea49886fd37ffa
thanhchi274 <anarapham274@gmail.com>
2021-04-15 18:37:42 +0700

    github pages deploy

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <Provider store={store}>
-  <BrowserRouter>
+  <BrowserRouter base="/Enterprise">
     <PersistGate persistor={persistor}>
       <App />
     </PersistGate>
