49593b2cafc05e8b31e1e1bd776a509ccc520f20
thanhchi274 <anarapham274@gmail.com>
2021-04-17 07:17:46 +0700

    config router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './Store/store';
@@ -9,11 +9,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <Provider store={store}>
-  <HashRouter basename="/" >
+  <BrowserRouter basename="https://thanhchi274.github.io/Enterprise" >
     <PersistGate persistor={persistor}>
       <App />
     </PersistGate>
-  </HashRouter >
+  </BrowserRouter >
 </Provider>,
   document.getElementById('root')
 );
